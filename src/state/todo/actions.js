94c8c1fc5d08e50d645e879b3b38c8d9bf394fd9
Yuri Maciel <54074819+yrimaciel@users.noreply.github.com>
2020-04-16 19:45:33 -0300

    todo tasks actions

---
+++ src/state/todo/actions.js
@@ -0,0 +1,40 @@
+import * as Types from './types'
+
+export function add(title) {
+  return {
+    type: Types.ADD,
+    payload: {
+      title
+    }
+  }
+}
+
+export function updateStatus(id, completed) {
+  return {
+    type: Types.TOGGLE_UPDATE_STATUS,
+    payload: {
+      id,
+      completed
+    }
+  }
+}
+
+export function updateTitle(id, title) {
+  return {
+    type: Types.TOGGLE_TITLE,
+    payload: {
+      id, 
+      title
+    }
+  }
+}
+
+export function remove(id) {
+  return {
+    type: Types.TOGGLE_UPDATE_STATUS,
+    payload: {
+      id
+    }
+  }
+}
+
---
+++ src/state/todo/types.js
@@ -0,0 +1,4 @@
+export const ADD = 'ADD'
+export const TOGGLE_UPDATE_STATUS = "TOGGLE_UPDATE_STATUS"
+export const TOGGLE_TITLE = "TOGGLE_TITLE"
+export const REMOVE = "REMOVE"
